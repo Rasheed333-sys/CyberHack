306e598fa94b479417cbfaffa592e8a78915bf78
Rasheed333-sys <skharoonrasheed2008@gmail.com>
2026-08-29 20:00:19 +0530

    Create src/vite-env.d.ts

---
+++ src/vite-env.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_AI_GATEWAY_URL: string;
+  readonly VITE_SEARCH_API_URL: string;
+  readonly VITE_WEB_RETRIEVAL_API_URL: string;
+  readonly VITE_PRIVACY_GATEWAY_URL: string;
+  readonly VITE_SECURITY_API_URL: string;
+  readonly VITE_AUTH_API_URL: string;
+  readonly VITE_USE_MOCK_SERVICES: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
